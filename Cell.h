5d026dbf8b4daaf30866d9a2e6e40b67776d234c
baz606 <baz6066@gmail.com>
2023-12-07 00:04:05 -0600

    Draw cells inside the grid. Add logo @baz606. Add README.md with instructions on how to build. Change raylib version to latest 5.0

--- Cell.h
+++ Cell.h
@@ -18,18 +18,22 @@ public:
   Cell();
   ~Cell();
 
-  Vector2Int* GetPosition() { return &position; }
-  Rectangle* GetRectangle() { return &rectangle; }
   void SetPosition(int x, int y);
   void SetRectangle(float x, float y, float width, float height);
 
+  Vector2Int* GetPosition() { return &position; }
+  Rectangle* GetRectangle() { return &rectangle; }
+  Color* GetColor() { return &color; }
+
+  // Length of this squared cell. Each cell will be the same length
+  static const int LENGTH = 100;
 
 private:
   // Index position in the 2D array of cells - easier to calculate adjacent cells
-  Vector2Int position;
+  Vector2Int position{};
   // The rectangle struct associated with this Cell that will be drawn on screen
   // Can be used to check for collision when a user selects this cell
-  Rectangle rectangle;
-  // Length of this squared cell -> This cell will be length x length
-  float length;
+  Rectangle rectangle{};
+  // Color for this cell
+  Color color;
 };
